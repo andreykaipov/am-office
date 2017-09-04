5c788942220c8fb7a7dfc8062630436f2b80e41a
Andrey Kaipov <akaip002@fiu.edu>
2017-09-04 02:43:01 -0400

    testing api calls static site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Http } from '@angular/http'
+import 'rxjs/Rx'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app'
+  planets = []
+
+  constructor(
+    private http: Http
+  ) {
+    this.getPlanets()
+  }
+
+  async getPlanets() {
+    for (let i = 1; i <= 10; i += 1) {
+      await this.wait(1000)
+      const json = this.http
+        .get(`https://swapi.co/api/planets/${i}/`)
+        .map(res => res.json())
+        .subscribe(planet => {
+          this.planets.push(planet.name)
+        })
+    }
+  }
+
+  async wait(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
 }
